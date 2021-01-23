b4c96b7915de5dcf5d0ca29d0cf6026557eb7472
zx <a@a.com>
2021-01-24 03:28:24 +0800

    add Dockerfile

--- src/release_linux.sh
+++ src/release_linux.sh
@@ -7,6 +7,8 @@ cp loginsight appdir/usr/bin/
 cp ../src/img/logo.png appdir/
 cp ../src/loginsight.desktop appdir/
 mkdir appdir/usr/lib
-../tool/linuxdeployqt appdir/usr/bin/loginsight -appimage
-mkdir artifact
+../tool/linuxdeployqt appdir/usr/bin/loginsight -appimage --appimage-extract-and-run
+if [ ! -d artifact ];then
+  mkdir artifact
+fi
 mv loginsight-*.AppImage artifact/loginsight.AppImage
